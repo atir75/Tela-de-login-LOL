498a372d0f8e5284524d5d33770cdf0f9adcefee
rita <maria.rita27012@gmail.com>
2022-06-02 15:43:33 -0300

    commit button arrow

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelectorAll('.login__button');
 
 const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
@@ -14,11 +15,22 @@ const handleFocusOut = ({ target }) => {
     }
 }
 
+const handleChange = () => {
+    const [username, password] = inputs;
+
+    if (username.value && password.value.length >= 8) {
+        button.removeAtribute('disabled');
+    }
+}
+
 inputs.forEach((input) => {
-    input.addEventListener('focus', handleFocus)
+    input.addEventListener('focus', handleFocus);
 });
 
 inputs.forEach((input) => {
-    input.addEventListener('focusout', handleFocusOut)
+    input.addEventListener('focusout', handleFocusOut);
 });
 
+inputs.forEach((input) => {
+    input.addEventListener('input', handleChange);
+});
